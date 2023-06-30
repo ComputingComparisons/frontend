74ff316a464b11aac280e0da6414e939ffecb75f
jakeRance <jake.rance01@gmail.com>
2023-06-30 13:11:57 -0500

    Made it so color is random and stored in firebase

--- src/firebase_setup/table.js
+++ src/firebase_setup/table.js
@@ -3,7 +3,7 @@ import { getFirestore,addDoc, collection, getDoc, doc, updateDoc, query, where, 
 
 // Add data to your "table" (collection) with headers, 3 columns, and the user's UID
 
-export const addDataToFirestore = async (userId, title="untitled") => {
+export const addDataToFirestore = async (userId, title="untitled", color) => {
   if (title == ""){
     title = "untitled"
   };
